518b879fb13f0f938856646ea2c17f91529179e2
cp1307 <65453191+cp1307@users.noreply.github.com>
2020-10-16 13:40:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var current = ["first", "second", "third"];
+var nextId;
+var activeId;
+
+$(".left-arrow").click(function() {
+  activeId = $(".active-text").attr("id");
+  console.log(activeId);
+  if (activeId === "f1")
+    nextId = "t3";
+  else if (activeId === "s2")
+    nextId = "f1";
+  else
+    nextId = "s2";
+
+  carousel(activeId, nextId);
+
+})
+$(".right-arrow").click(function() {
+  activeId = $(".active-text").attr("id");
+  console.log(activeId);
+  if (activeId === "f1")
+    nextId = "s2";
+  else if (activeId === "s2")
+    nextId = "t3";
+  else
+    nextId = "f1";
+
+  carousel(activeId, nextId);
+
+})
+
+function carousel(current, next) {
+  $("#" + current).removeClass("active-text").addClass("inactive-text");
+  $("#" + next).removeClass("inactive-text").addClass("active-text");
+
+  $("#bgc").removeClass("bgc-hero" + current.slice(1, 2)).addClass("bgc-hero" + next.slice(1, 2));
+
+}
+
+
+
+$(window).resize(function() {
+  var win = $(window).width();
+  console.log(win);
+  if(win <= 992)
+  {
+    $(".row").removeClass("row");
+    $(".col-7").removeClass("col-7");
+    $(".col-5").removeClass("col-5");
+  }
+})
